6acc576c50ace26baf12cdc3aa4e772fb811bf0c
Stelian Popa <developingtoday@gmail.com>
2021-06-22 13:57:38 +0300

    Add project files.

---
+++ BusinessRule/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRule.Model
+{
+    public abstract class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
